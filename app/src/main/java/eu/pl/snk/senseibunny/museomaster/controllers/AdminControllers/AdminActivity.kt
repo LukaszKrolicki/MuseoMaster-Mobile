a36523b5510f830ff674efbec667cbcb36ab2e37
SesneiBunny <lukasmatrix12345@gmail.com>
2024-02-06 13:33:41 +0100

    Admin Left menu added

---
+++ app/src/main/java/eu/pl/snk/senseibunny/museomaster/controllers/AdminControllers/AdminActivity.kt
@@ -0,0 +1,60 @@
+package eu.pl.snk.senseibunny.museomaster.controllers.AdminControllers
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Gravity
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import com.google.android.material.navigation.NavigationView
+import eu.pl.snk.senseibunny.museomaster.R
+import eu.pl.snk.senseibunny.museomaster.databinding.ActivityAdminBinding
+
+class AdminActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private  lateinit var fragmentManage: FragmentManager
+    private lateinit var binding: ActivityAdminBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding=ActivityAdminBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+        val toogle = ActionBarDrawerToggle(this,binding.drawerLayout, binding.toolbar,R.string.nav_open,R.string.nav_close)
+        binding.drawerLayout.addDrawerListener(toogle)
+        toogle.syncState()
+
+        fragmentManage = supportFragmentManager
+
+        binding.navigationDrawer.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.nav_add_user -> {openFragment(AddUserFragment())}
+            R.id.nav_add_room-> {openFragment(AddRoomFragment())}
+            R.id.nav_user_list->{openFragment(UserListFragment())}
+            R.id.nav_bugs->{openFragment(ReportFragment())}
+        }
+        binding.drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    override fun onBackPressed() {
+        if(binding.drawerLayout.isDrawerOpen(GravityCompat.START)){
+            binding.drawerLayout.closeDrawer(GravityCompat.START)
+        }
+        else{
+            super.getOnBackPressedDispatcher().onBackPressed()
+        }
+    }
+
+    private fun openFragment(fragment: Fragment){
+        val fragmentTransaction: FragmentTransaction = fragmentManage.beginTransaction()
+        fragmentTransaction.replace(R.id.fragment_container, fragment)
+        fragmentTransaction.commit()
+    }
+}
--- app/src/main/java/eu/pl/snk/senseibunny/museomaster/controllers/MainActivity.kt
+++ app/src/main/java/eu/pl/snk/senseibunny/museomaster/controllers/MainActivity.kt
@@ -1,20 +1,25 @@
 package eu.pl.snk.senseibunny.museomaster.controllers
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.security.ProviderInstaller
 
 import eu.pl.snk.senseibunny.museomaster.R
+import eu.pl.snk.senseibunny.museomaster.controllers.AdminControllers.AdminActivity
+import eu.pl.snk.senseibunny.museomaster.databinding.ActivityAdminBinding
+import eu.pl.snk.senseibunny.museomaster.databinding.ActivityMainBinding
 import eu.pl.snk.senseibunny.museomaster.models.DataBaseDriver
 import java.security.NoSuchAlgorithmException
 import javax.net.ssl.SSLContext
 
 class MainActivity : AppCompatActivity() {
     private lateinit var dataBaseDriver: DataBaseDriver
-
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         try {
             ProviderInstaller.installIfNeeded(getApplicationContext())
 
@@ -38,7 +43,11 @@ class MainActivity : AppCompatActivity() {
         }.start()
 
         System.out.println("xd")
-        setContentView(R.layout.activity_main)
+
+        binding.loginButton.setOnClickListener {
+            val intent = Intent(this, AdminActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
 
